11e2ac55189fce2fdc27a7c0ca5e8ce74097656f
Nargiza Konushalieva <64344753+Lebedok@users.noreply.github.com>
2020-11-25 22:56:03 -0600

    Add files via upload

---
+++ AnimalsLegs.java
@@ -0,0 +1,32 @@
+package HomeWork;
+
+public class AnimalsLegs {
+
+    public static void main(String[] args) {
+
+        int cows = 23;
+        int chickens = 42;
+        int cowsLegs = 4;
+        int chickenLegs = 2;
+        int totalLegs;
+
+        cowsLegs = cows * cowsLegs; // 23*4 =
+        chickenLegs = chickens * chickenLegs;
+        totalLegs = cowsLegs + chickenLegs;
+
+
+        int cowsPrice = 1253;
+        int chickensPrice = 11;
+        int totalPrice;
+
+        cowsPrice = cowsPrice * cows;
+        chickensPrice = chickensPrice * chickens;
+        totalPrice =  cowsPrice + chickensPrice;
+
+
+
+        System.out.println(totalLegs);
+        System.out.println(totalPrice);
+
+    }
+}
